2c0a6699a295d223087f7fccb0b967fb5796113d
Sivolapyy <mzhitnov@yandex.ru>
2022-04-13 17:00:07 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinOps bins = new BinOps();
+
+        System.out.println("Результат сложения: " + bins.sum("1101", "110"));
+        System.out.println("Результат умножения: " + bins.mult("1101", "110"));
+
+    }
+}
